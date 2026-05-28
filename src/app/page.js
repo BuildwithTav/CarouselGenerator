d37ead474b7434901e7b80f4a7834ba8ec06cf5a
BuildwithTav <runnerbean85@icloud.com>
2026-05-28 10:30:03 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client';
-import CarouselGenerator from '../../components/CarouselGenerator';
+import CarouselGenerator from '../components/CarouselGenerator';
 
 export default function Home() {
   return <CarouselGenerator />;
