99e7f8ffc5ac17c40debd001418368911e2ec83a
BuildwithTav <runnerbean85@icloud.com>
2026-06-06 22:57:18 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,5 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
   },
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
-    },
-  },
 }
 module.exports = nextConfig
