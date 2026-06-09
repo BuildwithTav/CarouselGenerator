7c8fa7ec93509a5f82cc16d14aa6a912b8ff97df
BuildwithTav <runnerbean85@icloud.com>
2026-06-09 18:53:49 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,15 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'no-store, must-revalidate' },
+        ],
+      },
+    ];
+  },
 }
 module.exports = nextConfig
