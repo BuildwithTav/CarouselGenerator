8270b6318e185ca7f9fa7708b7d5a13a76add8a1
BuildwithTav <runnerbean85@icloud.com>
2026-06-12 15:59:30 +0100

    Create page.js

---
+++ src/app/landing/page.js
@@ -0,0 +1,9 @@
+export default function LandingPage() {
+  return (
+    <iframe
+      src="/landing.html"
+      style={{width:'100%',height:'100vh',border:'none',display:'block'}}
+      title="Carousel Studio"
+    />
+  );
+}
