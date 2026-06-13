a3d683a866e3f48c2ea1df5b8e40871c06b64117
BuildwithTav <runnerbean85@icloud.com>
2026-06-13 19:26:58 +0100

    Create page.js

---
+++ src/app/affiliate/page.js
@@ -0,0 +1,9 @@
+export default function AffiliatePage() {
+  return (
+    <iframe
+      src="/affiliate.html"
+      style={{width:'100%',height:'100vh',border:'none',display:'block'}}
+      title="Carousel Studio Affiliate Programme"
+    />
+  );
+}
