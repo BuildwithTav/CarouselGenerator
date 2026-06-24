629362987cc50ab22e72b76d9fdbbe5edc7f4d61
BuildwithTav <runnerbean85@icloud.com>
2026-06-24 21:56:44 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,10 +6,16 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/manifest.json',
+        headers: [{ key: 'Cache-Control', value: 'public, max-age=86400' }],
+      },
+      {
+        source: '/icon-:size*.png',
+        headers: [{ key: 'Cache-Control', value: 'public, max-age=86400' }],
+      },
+      {
         source: '/:path*',
-        headers: [
-          { key: 'Cache-Control', value: 'no-store, must-revalidate' },
-        ],
+        headers: [{ key: 'Cache-Control', value: 'no-store, must-revalidate' }],
       },
     ];
   },
