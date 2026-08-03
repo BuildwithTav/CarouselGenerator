1ae246198491976f8ba2e704f83a8de3314df77f
BuildwithTav <runnerbean85@icloud.com>
2026-08-04 00:32:25 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,19 @@ export default function RootLayout({ children }) {
         <link rel="apple-touch-icon" href="/icon-192.png" />
         <meta name="theme-color" content="#000000" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
+        {/* Meta Pixel */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+          n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
+          (window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1897788154267737');
+          fbq('track', 'PageView');
+        `}} />
+        <noscript><img height="1" width="1" style={{display:'none'}}
+          src="https://www.facebook.com/tr?id=1897788154267737&ev=PageView&noscript=1"
+        /></noscript>
       </head>
       <body style={{ margin: 0, padding: 0, background: '#0D0D0D' }}>{children}</body>
     </html>
